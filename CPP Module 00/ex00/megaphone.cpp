609b78aada0392910045bae72e219e34c44f909d
Junkyu Park <junqwoopark@gmail.com>
2022-08-18 19:37:47 +0900

    ./CPP Module 00/ex00

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -6,13 +6,22 @@
 /*   By: junkpark <junkpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 16:23:20 by junkpark          #+#    #+#             */
-/*   Updated: 2022/08/17 16:52:41 by junkpark         ###   ########.fr       */
+/*   Updated: 2022/08/18 19:35:00 by junkpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(void) {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    if (argc == 1)
+        std::cout << "*LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else {
+        for (int i = 1; i < argc; i++) {
+            for (size_t j = 0; j < argv[i][j]; j++) {
+                std::cout << (char)std::toupper(argv[i][j]);
+            }
+        }
+        std::cout << std::endl;
+    }
     return (0);
 }
